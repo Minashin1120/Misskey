a55a3dd6dac69b6eeb6efedb7898bb1a41a113cc
misskey-admin <admin@mi.minashin1120.com>
2026-02-19 02:27:56 +0900

    feat(frontend): make suspended account message more prominent

--- packages/frontend/src/pages/user/home.vue
+++ packages/frontend/src/pages/user/home.vue
@@ -8,7 +8,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<div class="_spacer" :style="{ '--MI_SPACER-w': narrow ? '800px' : '1100px' }">
 		<div ref="rootEl" class="ftskorzw" :class="{ wide: !narrow }" style="container-type: inline-size;">
 			<div class="main _gaps">
-				<div class="punished _panel" v-if="user.isSuspended"><i class="ti ti-alert-triangle" style="margin-right: 8px;"></i> このアカウントは規約違反のため凍結されています。</div>
+				<div class="punished _panel isSuspended" v-if="user.isSuspended"><i class="ti ti-alert-triangle"></i><div>このアカウントは規約違反のため凍結されています。</div></div>
 				<div class="punished _panel" v-if="user.isSilenced && !shouldHideSuspendedProfileContent"><i class="ti ti-alert-triangle" style="margin-right: 8px;"></i> {{ i18n.ts.userSilenced }}</div>
 
 				<div v-if="!shouldHideSuspendedProfileContent" class="profile _gaps">
@@ -355,10 +355,24 @@ onDeactivated(disposeBannerParallaxResizeObserver);
 
 	> .main {
 
-		> .punished {
-			font-size: 0.8em;
-			padding: 16px;
-		}
+                > .punished {
+                        font-size: 1em;
+                        padding: 16px;
+                        text-align: center;
+
+                        &.isSuspended {
+                                font-size: 1.2em;
+                                font-weight: bold;
+                                padding: 64px 32px;
+                                color: var(--MI_THEME-error);
+
+                                > i {
+                                        display: block;
+                                        font-size: 4em;
+                                        margin: 0 0 16px 0 !important;
+                                }
+                        }
+                }
 
 		> .profile {
 
